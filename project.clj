01f902c4dc81f0ddb0e7b7fb7ae2b8f26a396709
Tom Savage <tcsavage@gmail.com>
2015-06-19 23:56:49 +0100

    Set project description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-mogensen-scott "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "ADTs in Clojure using Mogensen-Scott encoding"
+  :url "https://github.com/tcsavage/clj-mogensen-scott"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
